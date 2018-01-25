5106c0cea61611f547ea1c87ed5d16e80d50471f
James Gotsell <jamesgotsell@Jamess-MacBook-Pro.local>
2018-01-25 19:49:53 +0000

    wip updated package.json scripts to watch src folder and run flow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 // @flow
 console.log('boom')
 
+let quoteItem = [] 
+
+let getData = fetch('http://loremricksum.com/api/?paragraphs=4&quotes=3')
+      .then(response => response.json())
+      .then(data => {
+        return data
+})
+
+let stuff = () => {
+  return new Promise((resolve,reject) =>{
+    resolve(getData);
+  })
+}
+
+stuff().then( (data) =>{
+   console.log(data)
+   AddToDom(data)
+})
+.catch(function(error) {
+  console.log('oh no', error)
+})
+
+
+
 let root = document.getElementsByClassName('main-container')
 console.log(root)
 
-fetch('http://loremricksum.com/api/?paragraphs=4&quotes=3')
-      .then(response => response.json())
-      .then(data => {
-        let quotes = data
-        quotes.data.map(( quote , i ) => {
-            console.log(quote)
-           // within this 
-        })
-        
-      })
+const AddToDom = (quoteArray:Array<string>) => {
+  let root = document.getElementsByClassName('main-container')
+
+  console.log(quoteArray)
+  quoteArray.map( el => { 
+    console.log(el) 
+    return el
+  })
+}
 
